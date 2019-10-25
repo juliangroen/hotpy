dfbdcf611b5f4ac0e2164454d72b642219fe6b8f
Foolz <akufoolz@gmail.com>
2019-10-24 21:51:00 -0600

    now pulling exit combo from config file

--- hotpy.py
+++ hotpy.py
@@ -8,14 +8,19 @@ combo = set()
 keys = []
 
 def getConfigFile():
+
     pwd = os.path.dirname(sys.argv[0])
     pathFile = os.path.join(pwd, "config.json")
+
     with open(pathFile, 'r') as f:
         cfg = json.load(f)
+
     return cfg
 
 def getHotkeysFromFile():
+
     cfg = getConfigFile()
+
     if not cfg["altDir"]:
         pwd = os.path.dirname(sys.argv[0])
         pathFile = os.path.join(pwd, cfg["hotkeyFile"]) 
@@ -25,9 +30,8 @@ def getHotkeysFromFile():
 
     with open(pathFile) as hkFile:
         hks = dict(line.strip().split("::", 1) for line in hkFile)
-    return hks
 
-hkDict = getHotkeysFromFile()
+    return hks
 
 def on_press(key):
 
@@ -48,12 +52,20 @@ def on_press(key):
     
 def on_release(key):
 
+    cfg = getConfigFile()
+
+    if (set([str(x)[4:] for x in combo]) == set(cfg["exitCombo"].split(", ")) or
+        set([str(x)[4:] for x in combo]) == set(cfg["macExitCombo"].split(", "))):
+        print("exiting...")
+        return False
+
     if key in combo:
         combo.remove(key)
 
 def checkForHotkey():
+
     hk = "".join(keys)
-    match = hkDict.get(hk)
+    match = getHotkeysFromFile().get(hk)
 
     if match: 
         for _ in range(len(keys) + 1):
@@ -69,7 +81,20 @@ def checkForHotkey():
     
     keys.clear()
 
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+def exitComboPrint():
+
+    if sys.platform == "win32":
+        print("Press {} together to exit.".format(getConfigFile()["exitCombo"]))
+    elif sys.platform == "darwin":
+        print("Press {} together to exit.".format(getConfigFile()["macExitCombo"]))
+
+def main():
+
+    exitComboPrint()
+    with Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
